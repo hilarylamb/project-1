6d99881ffac2f213a06ec8ebaa1fdafe6038a92d
Hilary Lamb <hilaryklamb@gmail.com>
2020-06-16 19:19:37 -0400

    text array

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
 let pageNumber = 0;
 
+// content for pages
+
+const pages = ['brooklyn design', 'kanye west', 'october', 'download my pdf'];
+
+//pick the relevent tags
 const nextTag = document.querySelector('footer img.next');
 const previousTag = document.querySelector('footer img.prev');
 const outputTag = document.querySelector('h2');
 
+// make a next function to increase page number
 const next = function () {
   pageNumber = pageNumber + 1;
 
-  if (pageNumber > 10) {
+  if (pageNumber > pages.length - 1) {
     pageNumber = 1;
   }
 
@@ -17,15 +23,15 @@ const next = function () {
 const previous = function () {
   pageNumber = pageNumber - 1;
 
-  if (pageNumber < 1) {
-    pageNumber = 10;
+  if (pageNumber < pages.length - 1) {
+    pageNumber = 3;
   }
 
   updateSection();
 };
 
 const updateSection = function () {
-  outputTag.innerHTML = pageNumber;
+  outputTag.innerHTML = pages[pageNumber];
 };
 
 nextTag.addEventListener('click', function () {
