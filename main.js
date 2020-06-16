11582732db4b251eb9380bdfb1a074c22ca5db34
Hilary Lamb <hilaryklamb@gmail.com>
2020-06-16 18:51:55 -0400

    done with next functions

--- main.js
+++ main.js
@@ -3,8 +3,20 @@ let pageNumber = 0;
 const nextTag = document.querySelector('footer img.next');
 const outputTag = document.querySelector('h2');
 
-nextTag.addEventListener('click', function () {
+const next = function () {
   pageNumber = pageNumber + 1;
 
+  if (pageNumber > 10) {
+    pageNumber = 1;
+  }
+
+  updateSection();
+};
+
+const updateSection = function () {
   outputTag.innerHTML = pageNumber;
+};
+
+nextTag.addEventListener('click', function () {
+  next();
 });
