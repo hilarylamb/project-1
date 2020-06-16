f8c38a7e430dd6ea29bf4a2869274401ac8ad229
Hilary Lamb <hilaryklamb@gmail.com>
2020-06-16 19:03:59 -0400

    previous function working

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let pageNumber = 0;
 
 const nextTag = document.querySelector('footer img.next');
+const previousTag = document.querySelector('footer img.prev');
 const outputTag = document.querySelector('h2');
 
 const next = function () {
@@ -13,10 +14,24 @@ const next = function () {
   updateSection();
 };
 
+const previous = function () {
+  pageNumber = pageNumber - 1;
+
+  if (pageNumber < 1) {
+    pageNumber = 10;
+  }
+
+  updateSection();
+};
+
 const updateSection = function () {
   outputTag.innerHTML = pageNumber;
 };
 
 nextTag.addEventListener('click', function () {
   next();
+});
+
+previousTag.addEventListener('click', function () {
+  previous();
 });
